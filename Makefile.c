331c6aa8cc3e21c5af8ea618abbe7187baa75a72
Bruno Barros <fcportobb@gmail.com>
2025-11-09 14:52:08 +0000

    Makefile

---
+++ Makefile.c
@@ -0,0 +1,23 @@
+# Makefile for building 'calc' from parser.y, lexer.x and main.c
+
+BISON = bison
+FLEX  = flex
+CC    = gcc
+CFLAGS = -lm
+
+TARGET = calc
+BISON_SRC = parser.y
+FLEX_SRC  = lexer.x
+SRCS = main.c
+
+all: $(TARGET)
+
+$(TARGET): $(BISON_SRC) $(FLEX_SRC) $(SRCS)
+	$(BISON) -d $(BISON_SRC)
+	$(FLEX) $(FLEX_SRC)
+	$(CC) -o $(TARGET) parser.tab.c lex.yy.c $(SRCS) $(CFLAGS)
+
+clean:
+	rm -f $(TARGET) parser.tab.c parser.tab.h lex.yy.c
+
+.PHONY: all clean
